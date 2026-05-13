f4db41413f9873c8daf1ea701cd9bb97911b233b
phanmemquanli <tulieuhopthanh@gmail.com>
2026-05-13 17:03:05 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -1,7 +1,9 @@
 /**************************************************
  * TỆP KẾT NỐI (ketnoi.js)
- * Lĩnh vực: Quản lý địa chỉ kết nối về Máy chủ Google.
- * Nguyên tắc: Chỉ chứa duy nhất đường link Web App (đuôi /exec).
+ * Lĩnh vực: Quản lý địa chỉ kết nối và Giao diện UI.
+ * Nguyên tắc: Chỉ chứa link Web App, Tên trường, Link Logo.
  **************************************************/
 
 var SCV_CONFIG_GAS_URL = "https://script.google.com/macros/s/AKfycbxQNLrkoHnxF81IO_QmB7rcIdjQhavLWNG6fy3VngBeR_2xG8rHxJAlrJLcAs_szZO9/exec";
+var SCV_TEN_TRUONG = "TRƯỜNG TH&THCS HỢP THÀNH";
+var SCV_LINK_LOGO = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"; // Đồng chí thay thế bằng URL ảnh Logo thực tế (đuôi .png/.jpg)
