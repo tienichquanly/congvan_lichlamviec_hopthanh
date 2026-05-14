e8496760b3a21a98478a0038beeab1179c6cc3dc
phanmemquanli <tulieuhopthanh@gmail.com>
2026-05-14 12:31:00 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -5,7 +5,7 @@
  **************************************************/
 
 var SCV_CONFIG_GAS_URL = "https://script.google.com/macros/s/AKfycbxQNLrkoHnxF81IO_QmB7rcIdjQhavLWNG6fy3VngBeR_2xG8rHxJAlrJLcAs_szZO9/exec";
-var SCV_LINK_LOGO = "https://i.ibb.co/6R8Y9DNq/logo-l-a-ch-n6.png"; // Đồng chí thay thế bằng URL ảnh Logo thực tế (đuôi .png/.jpg)
+var SCV_LINK_LOGO = "https://i.ibb.co/6R8Y9DNq/logo-l-a-ch-n6.png"; 
 
 var SCV_TEN_TRUONG = "TRƯỜNG TH&THCS HỢP THÀNH";
 var SCV_TEN_TRUONG_UP = "TRƯỜNG TH&THCS HỢP THÀNH";
@@ -16,6 +16,9 @@ var SCV_PHIEN_BAN = "V2.5.2026";
 var SCV_TAC_GIA = "Hoàng Ngọc Lâm";
 var SCV_TAC_GIA_UP = "HOÀNG NGỌC LÂM";
 
+// BỔ SUNG: BIẾN MÔ TẢ HỆ THỐNG DÙNG ĐỂ SEO/NHẬN DIỆN WEB
+var SCV_MO_TA = "Hệ thống quản lý công văn và lịch công tác nội bộ - Trường TH&THCS Hợp Thành.";
+
 // MÃ CLIENT ID CỦA GOOGLE ĐĂNG NHẬP
 var SCV_GOOGLE_CLIENT_ID = "407480994586-m6fpq6sfcc90qqj9k08rsmi1lge6br94.apps.googleusercontent.com";
 
@@ -23,17 +26,13 @@ var SCV_GOOGLE_CLIENT_ID = "407480994586-m6fpq6sfcc90qqj9k08rsmi1lge6br94.apps.g
 /* KHỞI TẠO CẤU TRÚC HEADER TỰ ĐỘNG TỪ FILE JS                               */
 /* ========================================================================= */
 (function() {
-    var metaCharset = document.createElement('meta');
-    metaCharset.setAttribute('charset', 'utf-8');
-    document.head.appendChild(metaCharset);
-
-    document.title = SCV_TEN_PM + " - " + SCV_TEN_TRUONG;
-    
+    // 1. Sinh thẻ Meta Description động
     var metaDesc = document.createElement('meta');
     metaDesc.name = "description";
-    metaDesc.content = "Hệ thống " + SCV_TEN_PM.toLowerCase() + " nội bộ - " + SCV_TEN_TRUONG + ".";
+    metaDesc.content = SCV_MO_TA;
     document.head.appendChild(metaDesc);
 
+    // 2. Sinh thẻ Link Icon (Favicon) động
     var linkIcon = document.createElement('link');
     linkIcon.rel = 'icon';
     linkIcon.href = SCV_LINK_LOGO;
