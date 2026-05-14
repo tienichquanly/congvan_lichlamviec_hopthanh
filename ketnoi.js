f44920b56d6003edafe1d7a71cc690f3e13f587e
phanmemquanli <tulieuhopthanh@gmail.com>
2026-05-14 11:42:35 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -5,7 +5,7 @@
  **************************************************/
 
 var SCV_CONFIG_GAS_URL = "https://script.google.com/macros/s/AKfycbxQNLrkoHnxF81IO_QmB7rcIdjQhavLWNG6fy3VngBeR_2xG8rHxJAlrJLcAs_szZO9/exec";
-var SCV_LINK_LOGO = "https://i.ibb.co/6R8Y9DNq/logo-l-a-ch-n6.png"; // Đồng chí thay thế bằng URL ảnh Logo thực tế (đuôi .png/.jpg)
+//var SCV_LINK_LOGO = "https://i.ibb.co/6R8Y9DNq/logo-l-a-ch-n6.png"; // Đồng chí thay thế bằng URL ảnh Logo thực tế (đuôi .png/.jpg)
 
 var SCV_TEN_TRUONG = "TRƯỜNG TH&THCS HỢP THÀNH";
 var SCV_TEN_TRUONG_UP = "TRƯỜNG TH&THCS HỢP THÀNH";
